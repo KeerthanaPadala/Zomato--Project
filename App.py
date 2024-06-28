1cc90d840787b49e247e2a0677bd471cbe2eafe0
KeerthanaPadala <keerthanarao1010@gmail.com>
2024-06-28 17:26:19 +0530

    anything

---
+++ App.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/recommend")
+def recommend():
+    return render_template("recommend.html")
+
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=6500)
+
